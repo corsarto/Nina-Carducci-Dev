d54e207c4bf0807cb427f2be924d7f78da05ea8f
corsarto <clerc.loic@gmail.com>
2025-05-12 16:06:57 +0200

    Adjust SEO with schema.org

---
+++ gallery.js
@@ -0,0 +1,66 @@
+const gallery = document.querySelector('.gallery');
+gallery.setAttribute('style', 'display: flex;');
+
+const galleryWrapper = document.querySelector('.container-gallery');
+const galleryFilter = document.createElement('div');
+galleryFilter.setAttribute('class', 'gallery-filter');
+
+const filterAll = document.createElement('button');
+filterAll.textContent = 'Tous';
+filterAll.setAttribute('class', 'filter-button active ');
+filterAll.setAttribute('data-gallery-tag', 'all');
+
+
+const filterConcert = document.createElement('button');
+filterConcert.textContent = 'Concert';
+filterConcert.setAttribute('class', 'filter-button');
+filterConcert.setAttribute('data-gallery-tag', 'Concert');
+
+const filterEntreprises = document.createElement('button');
+filterEntreprises.textContent = 'Entreprises';
+filterEntreprises.setAttribute('class', 'filter-button');
+filterEntreprises.setAttribute('data-gallery-tag', 'Entreprises');
+
+const filterWedding = document.createElement('button');
+filterWedding.textContent = 'Mariages';
+filterWedding.setAttribute('class', 'filter-button');
+filterWedding.setAttribute('data-gallery-tag', 'Mariages');
+
+const filterPortrait = document.createElement('button');
+filterPortrait.textContent = 'Portrait';
+filterPortrait.setAttribute('class', 'filter-button');
+filterPortrait.setAttribute('data-gallery-tag', 'Portrait');
+
+galleryWrapper.appendChild(galleryFilter);
+galleryFilter.appendChild(filterAll);
+galleryFilter.appendChild(filterConcert);
+galleryFilter.appendChild(filterEntreprises);
+galleryFilter.appendChild(filterWedding);
+galleryFilter.appendChild(filterPortrait);
+galleryWrapper.appendChild(gallery);
+
+
+const filterButtons = document.querySelectorAll('.filter-button');
+const galleryContent = document.querySelectorAll('.gallery-item');
+
+const filterButtonCard = (e) => {
+    document.querySelector('.filter-button.active').classList.remove('active');
+    e.target.classList.add('active');
+
+    galleryContent.forEach((item) => {
+        if (item.dataset.galleryTag === e.target.dataset.galleryTag || e.target.dataset.galleryTag === 'all') {
+            item.style.display = 'flex';
+        }
+        else {
+            item.style.display = 'none';
+
+        }
+})};
+filterButtons.forEach(button => button.addEventListener('click', (filterButtonCard)));
+
+
+   
+
+
+
+
