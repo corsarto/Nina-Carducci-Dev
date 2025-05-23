6d17ced84105ed91ea393bfba30579080e9c90db
corsarto <clerc.loic@gmail.com>
2025-05-23 13:35:19 +0200

    Final version

--- gallery.js
+++ gallery.js
@@ -40,41 +40,47 @@ galleryWrapper.appendChild(gallery);
 
 const filterButtons = document.querySelectorAll('.filter-button');
 const galleryContent = document.querySelectorAll('.gallery-item');
+let galleryFiltered = [];
+
+function displayAll() {
+    if (galleryFiltered.length === 0) {
+        galleryFiltered = Array.from(galleryContent);
+    }}
+displayAll();
+function displayButtonFilter(e) {
+    for (let i = 0; i < galleryContent.length; i++) {
+        const item = galleryContent[i];
+        const tag = item.getAttribute('data-gallery-tag');
+        
+        item.style.display = (tag === e.target.dataset.galleryTag || e.target.dataset.galleryTag === 'all') ? 'flex' : 'none';}
+        filteredGalleryContent();
+    }
+function filteredGalleryContent() {
+    galleryFiltered = Array.from(galleryContent).filter(item => item.style.display !== 'none');
+    }   
 
 
 
 const filterButtonCard = (e) => {
     document.querySelector('.filter-button.active-type').classList.remove('active-type');
     e.target.classList.add('active-type');
-    
-    galleryContent.forEach((item) => {
-        if (item.dataset.galleryTag === e.target.dataset.galleryTag || e.target.dataset.galleryTag === 'all') {
-            item.style.display = 'flex';
-            
-            }
-        else {
-            item.style.display = 'none';
-            
-}
-})};
+    displayButtonFilter(e);};
+
 filterButtons.forEach(button => button.addEventListener('click', filterButtonCard));
 
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.img-modal-content img');
 const imgModal = document.querySelector('.img-modal');
 
-let ImageSrc = document.querySelectorAll('.gallery-item img');
-ImageSrc = Array.from(galleryContent).map((img) => img.getAttribute('src'));
 
 
 galleryContent.forEach((item) => {
     item.addEventListener('click', () => {
     const imgSrc = item.getAttribute('src'); 
     imgModal.setAttribute('src',imgSrc);  
-    modal.style.display = 'flex'; })}
-        
-        
-);
+    modal.style.display = 'flex'; 
+
+});});
 
 modal.addEventListener('click', (e) => {
     if (!e.target.closest('.img-modal-content')) {
@@ -85,29 +91,32 @@ modal.addEventListener('click', (e) => {
 const rigthBtn = document.querySelector('#button-right');
 const leftBtn = document.querySelector('#button-left');
 
+
+
+function changeImageModal(e) {
+    let currentSrc = modalContent.getAttribute('src');
+    let currentIndex = Array.from(galleryFiltered).findIndex(item => item.getAttribute('src') === currentSrc);
+    let nextIndex;
+  
+    
+
+    if (e === rigthBtn) {
+        nextIndex = (currentIndex + 1) % galleryFiltered.length;
+    } else if (e === leftBtn) {
+        nextIndex = (currentIndex - 1 + galleryFiltered.length) % galleryFiltered.length;
+    }
+
+    let nextImg = galleryFiltered[nextIndex].getAttribute('src');
+    modalContent.setAttribute('src', nextImg);
+   
+}
+
+
 rigthBtn.addEventListener('click', (e) => {
     e.stopPropagation();
-    
-    const currentSrc = modalContent.getAttribute('src');
-    const currentIndex = Array.from(galleryContent).findIndex(item => item.getAttribute('src') === currentSrc);
-    const nextIndex = (currentIndex + 1) % galleryContent.length;
-    const nextImg = galleryContent[nextIndex].getAttribute('src');
-    modalContent.setAttribute('src', nextImg);
+  changeImageModal(rigthBtn);});
 
-});
 
 leftBtn.addEventListener('click', (e) => {
     e.stopPropagation();
-    
-    const currentSrc = modalContent.getAttribute('src');
-    const currentIndex = Array.from(galleryContent).findIndex(item => item.getAttribute('src') === currentSrc);
-    const nextIndex = (currentIndex - 1 + galleryContent.length) % galleryContent.length;
-    const nextImg = galleryContent[nextIndex].getAttribute('src');
-    modalContent.setAttribute('src', nextImg);
-    
-});
-
-
-
-
-
+changeImageModal(leftBtn);});
